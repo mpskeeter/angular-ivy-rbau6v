c1df854f3274a640adb175916db340a1a255cd96
mpskeeter <mpskeeter@gmail.com>
2022-02-22 09:52:02 -0500

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayoutComponent } from '@layout';
+// import { LayoutComponent } from './modlayout';
 
 const routes: Routes = [
   // {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 // import { SharedModule, LayoutModule } from './modules';
 
 import { AppComponent } from './app.component';
